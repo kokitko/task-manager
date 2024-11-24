ce65fcf37680dcced0b52e6b6b0b51d8a7d783c6
kokitko <koks.vladislav277@gmail.com>
2024-11-24 16:00:05 +0100

    implementing basic spring security (p2)

---
+++ src/main/java/com/project/task_manager/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.project.task_manager.controller;
+
+import com.project.task_manager.dto.RegisterRequest;
+import com.project.task_manager.entity.UserEntity;
+import com.project.task_manager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public AuthController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody RegisterRequest registerRequest) {
+        if (userRepository.existsByEmail(registerRequest.getEmail()) ||
+        userRepository.existsByUsername(registerRequest.getUsername())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body
+                    ("User with this email or username already exists");
+        }
+        UserEntity user = new UserEntity();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setRole(UserEntity.Role.USER);
+        userRepository.save(user);
+
+        return ResponseEntity.ok("User registered successfully");
+    }
+}
