4333abf5150c8ecbc6c5fbcef673508342b9d2f7
kokitko <koks.vladislav277@gmail.com>
2024-11-24 22:08:15 +0100

    secure data by injecting ProjectDtos

--- src/main/java/com/project/task_manager/service/ProjectService.java
+++ src/main/java/com/project/task_manager/service/ProjectService.java
@@ -1,13 +1,14 @@
 package com.project.task_manager.service;
 
-import com.project.task_manager.entity.Project;
+import com.project.task_manager.dto.ProjectRequestDto;
+import com.project.task_manager.dto.ProjectResponseDto;
 import com.project.task_manager.entity.UserEntity;
 
 import java.util.List;
 
 public interface ProjectService {
-    Project createProject(Project project, UserEntity user);
-    List<Project> getProjectsByUser(UserEntity user);
-    Project updateProject(Long projectId, Project project);
+    ProjectResponseDto createProject(ProjectRequestDto project, UserEntity user);
+    List<ProjectResponseDto> getProjectsByUser(UserEntity user);
+    ProjectResponseDto updateProject(Long projectId, ProjectRequestDto project);
     void deleteProject(Long projectId);
 }
