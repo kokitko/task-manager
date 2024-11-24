12b06fee2f1df3857368c6ccc36d31e6270c3d93
kokitko <koks.vladislav277@gmail.com>
2024-11-24 18:47:48 +0100

    fix + secure sensitive info

---
+++ src/main/java/com/project/task_manager/util/SecurityConstants.java
@@ -0,0 +1,32 @@
+package com.project.task_manager.util;
+
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.PrePersist;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class SecurityConstants {
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
+    @Value("${jwt.expiration}")
+    protected long EXPIRATION_TIME;
+
+    protected Key KEY;
+
+    @PostConstruct
+    public void init() {
+        this.KEY = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public long getEXPIRATION_TIME() {
+        return EXPIRATION_TIME;
+    }
+
+    public Key getKEY() {
+        return KEY;
+    }
+}
