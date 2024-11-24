066632d5b52e567a4827aeb8dcdd9d69882ce54b
kokitko <koks.vladislav277@gmail.com>
2024-11-24 16:20:24 +0100

    configuring JWT authorization (p1)

---
+++ src/main/java/com/project/task_manager/dto/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.project.task_manager.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/project/task_manager/util/JwtUtils.java
@@ -0,0 +1,46 @@
+package com.project.task_manager.util;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+    private static final String SECRET_KEY = "REMOVED";
+    private static final long EXPIRATION_TIME = REMOVED;
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException ex) {
+            return false;
+        }
+    }
+}
