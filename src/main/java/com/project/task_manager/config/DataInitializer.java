1be6ed7106859954187569536497a72d9e467f77
kokitko <koks.vladislav277@gmail.com>
2024-11-26 22:57:47 +0100

    admin initializing

--- src/main/java/com/project/task_manager/config/DataInitializer.java
+++ src/main/java/com/project/task_manager/config/DataInitializer.java
@@ -1,16 +1,19 @@
 package com.project.task_manager.config;
 
 import com.project.task_manager.entity.Role;
+import com.project.task_manager.entity.UserEntity;
 import com.project.task_manager.repository.RoleRepository;
+import com.project.task_manager.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class DataInitializer {
 
     @Bean
-    CommandLineRunner initRoles(RoleRepository roleRepository) {
+    CommandLineRunner initRolesAndAdmin(RoleRepository roleRepository, UserRepository userRepository) {
         return args -> {
             if (!roleRepository.findByAuthority("ROLE_USER").isPresent()) {
                 Role userRole = new Role();
@@ -21,6 +24,16 @@ public class DataInitializer {
                 Role adminRole = new Role();
                 adminRole.setAuthority("ROLE_ADMIN");
                 roleRepository.save(adminRole);
+            }
+            if (!userRepository.existsByUsername("admin")) {
+                Role adminRole = roleRepository.findByAuthority("ROLE_ADMIN")
+                        .orElseThrow(() -> new RuntimeException("Role not found"));
+                UserEntity admin = new UserEntity();
+                admin.setUsername("admin");
+                admin.setEmail("admin");
+                admin.setPassword(new BCryptPasswordEncoder().encode("admin"));
+                admin.setRole(adminRole);
+                userRepository.save(admin);
             }
         };
     }
