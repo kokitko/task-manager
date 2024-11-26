9662fc6825d1646d357bcd670f9065a73e509a17
Vladyslav Kokitko <kokitko.vladyslav@gmail.com>
2024-11-26 19:01:56 +0100

    create and handle TaskNotFoundException

--- src/main/java/com/project/task_manager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/task_manager/exception/GlobalExceptionHandler.java
@@ -20,4 +20,15 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorObject, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<ErrorObject> handleTaskNotFoundException() {
+        ErrorObject errorObject = new ErrorObject();
+
+        errorObject.setStatusCode(404);
+        errorObject.setMessage("Task not found");
+        errorObject.setTimestamp(new Date());
+
+        return new ResponseEntity<>(errorObject, HttpStatus.NOT_FOUND);
+    }
 }
