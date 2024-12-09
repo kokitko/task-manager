fe987bf6cbddcc1e16b1ef32c510f733616d9053
kokitko <koks.vladislav277@gmail.com>
2024-12-09 23:26:36 +0100

    project repository tests (pt. 1)

--- src/test/java/com/project/task_manager/repository/ProjectRepositoryTest.java
+++ src/test/java/com/project/task_manager/repository/ProjectRepositoryTest.java
@@ -4,6 +4,7 @@ import com.project.task_manager.entity.Project;
 import com.project.task_manager.entity.UserEntity;
 import jakarta.transaction.Transactional;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -20,31 +21,61 @@ public class ProjectRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
-    @Test
-    public void ProjectRepository_SaveAllProjects_ReturnsAllProjects() {
-        UserEntity user = UserEntity.builder()
-                .username("testusername")
-                .email("testemail")
-                .password("testpassword")
-                .build();
+    UserEntity user = new UserEntity();
+    Project project1 = new Project();
+    Project project2 = new Project();
 
+    @BeforeEach
+    public void init() {
+        user.setUsername("testusername");
+        user.setEmail("testemail");
+        user.setPassword("testpassword");
         userRepository.save(user);
 
-        Project project1 = Project.builder()
-                .name("testproject1")
-                .description("testdescription1")
-                .user(user)
-                .build();
+        project1.setName("testproject1");
+        project1.setDescription("testdescription1");
+        project1.setUser(user);
 
-        Project project2 = Project.builder()
-                .name("testproject2")
-                .description("testdescription2")
-                .user(user)
-                .build();
+        project2.setName("testproject2");
+        project2.setDescription("testdescription2");
+        project2.setUser(user);
+    }
 
+    @Test
+    public void ProjectRepository_SaveAllProjects_ReturnsAllProjects() {
         List<Project> list = projectRepository.saveAll(List.of(project1, project2));
 
         Assertions.assertThat(list).isNotNull();
         Assertions.assertThat(list.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void ProjectRepository_SaveProject_ReturnsProject() {
+        Project project = projectRepository.save(project1);
+
+        Assertions.assertThat(project).isNotNull();
+        Assertions.assertThat(project.getId()).isNotNull();
+    }
+
+    @Test
+    public void ProjectRepository_FindByUser_ReturnsUserProjects() {
+        projectRepository.saveAll(List.of(project1, project2));
+
+        List<Project> list = projectRepository.findByUser(user);
+
+        Assertions.assertThat(list).isNotNull();
+        Assertions.assertThat(list.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void ProjectRepository_FindById_ReturnsProject() {
+        Project savedProject = projectRepository.save(project1);
+
+        Project project = projectRepository.findById(savedProject.getId()).orElseThrow(
+                () -> new IllegalArgumentException("Project not found")
+        );
+
+        Assertions.assertThat(project).isNotNull();
+        Assertions.assertThat(project.getId()).isEqualTo(project1.getId());
     }
 }
