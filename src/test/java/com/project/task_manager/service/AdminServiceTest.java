3689b6eaf0817a7456b1bde2a721c6e0b29ba992
kokitko <koks.vladislav277@gmail.com>
2024-12-13 14:44:22 +0100

    AdminService tests (crud projects)

--- src/test/java/com/project/task_manager/service/AdminServiceTest.java
+++ src/test/java/com/project/task_manager/service/AdminServiceTest.java
@@ -1,19 +1,28 @@
 package com.project.task_manager.service;
 
 import com.project.task_manager.dto.ProjectRequestDto;
+import com.project.task_manager.dto.ProjectResponseDto;
 import com.project.task_manager.dto.TaskRequestDto;
 import com.project.task_manager.entity.Project;
-import com.project.task_manager.entity.Role;
 import com.project.task_manager.entity.Task;
 import com.project.task_manager.entity.UserEntity;
 import com.project.task_manager.repository.ProjectRepository;
 import com.project.task_manager.repository.TaskRepository;
 import com.project.task_manager.service.impl.AdminServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AdminServiceTest {
@@ -35,6 +44,7 @@ public class AdminServiceTest {
         user.setUsername("testuser");
         user.setEmail("testemail");
         user.setPassword("testpassword");
+        user.setId(1L);
 
         projectRequestDto.setName("testprojectdto");
         projectRequestDto.setDescription("testdescriptionto");
@@ -51,5 +61,38 @@ public class AdminServiceTest {
         task.setName("testtask");
         task.setDescription("testdescription");
         task.setProject(project);
+    }
+
+    @Test
+    public void AdminService_CreateProject_ReturnsProjectResponseDto() {
+        when(projectRepository.save(Mockito.any(Project.class))).thenReturn(project);
+
+        ProjectResponseDto projectResponseDto = adminService.createProject(projectRequestDto, user);
+        Assertions.assertThat(projectResponseDto.getName()).isEqualTo(project.getName());
+    }
+
+    @Test
+    public void AdminService_GetProjectsByUser_ReturnsProjectResponseDtoList() {
+        when(projectRepository.findByUser(Mockito.any(UserEntity.class))).thenReturn(List.of(project));
+
+        List<ProjectResponseDto> projectResponseDtoList = adminService.getProjectsByUser(user);
+        Assertions.assertThat(projectResponseDtoList).hasSize(1);
+    }
+
+    @Test
+    public void AdminService_UpdateProject_ReturnsProjectResponseDto() {
+        when(projectRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(project));
+        when(projectRepository.save(Mockito.any(Project.class))).thenReturn(project);
+
+        ProjectResponseDto projectResponseDto = adminService.updateProject
+                (project.getId(), projectRequestDto, user.getId());
+        Assertions.assertThat(projectResponseDto.getName()).isEqualTo(projectRequestDto.getName());
+    }
+
+    @Test
+    public void AdminService_DeleteProject_ReturnsVoid() {
+        when(projectRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(project));
+
+        assertAll(() -> adminService.deleteProject(project.getId(), user.getId()));
     }
 }
