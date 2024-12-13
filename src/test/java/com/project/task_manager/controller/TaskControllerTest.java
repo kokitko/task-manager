736107f8f8c8afadd8eb8e98b2af9cdf48e6b2f8
kokitko <koks.vladislav277@gmail.com>
2024-12-13 20:02:39 +0100

    TaskController tests (2/4)

--- src/test/java/com/project/task_manager/controller/TaskControllerTest.java
+++ src/test/java/com/project/task_manager/controller/TaskControllerTest.java
@@ -8,14 +8,19 @@ import com.project.task_manager.entity.Project;
 import com.project.task_manager.entity.UserEntity;
 import com.project.task_manager.service.TaskService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -48,12 +53,33 @@ public class TaskControllerTest {
         project.setUser(user);
 
         taskRequestDto = new TaskRequestDto();
-        taskRequestDto.setName("testtask");
+        taskRequestDto.setName("testtaskrequest");
         taskRequestDto.setDescription("testdescription");
 
         taskResponseDto = new TaskResponseDto();
         taskResponseDto.setId(1L);
-        taskResponseDto.setName("testtask");
+        taskResponseDto.setName("testtaskresponse");
         taskResponseDto.setDescription("testdescription");
+    }
+
+    @Test
+    public void TaskController_CreateTask_ReturnsTaskResponseDto() throws Exception {
+        when(taskService.createTask(any(TaskRequestDto.class), any(Long.class))).thenReturn(taskResponseDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/project/1")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(taskRequestDto)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("testtaskresponse"));
+    }
+
+    @Test
+    public void TaskController_GetTasksByProject_ReturnsTaskResponseDtoList() throws Exception {
+        when(taskService.getTasksByProjectId(any(Long.class))).thenReturn(List.of(taskResponseDto));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/project/1")
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("testtaskresponse"));
     }
 }
