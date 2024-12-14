3f1153b8d520d498d7c0d7f4ba8a749156125069
kokitko <koks.vladislav277@gmail.com>
2024-12-14 15:51:06 +0100

    AuthController tests

--- src/test/java/com/project/task_manager/controller/AuthControllerTest.java
+++ src/test/java/com/project/task_manager/controller/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package com.project.task_manager.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.task_manager.dto.LoginRequest;
 import com.project.task_manager.dto.RegisterRequest;
 import com.project.task_manager.entity.Role;
@@ -8,11 +9,29 @@ import com.project.task_manager.repository.RoleRepository;
 import com.project.task_manager.repository.UserRepository;
 import com.project.task_manager.util.JwtUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
 public class AuthControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
     @Mock
     private RoleRepository roleRepository;
     @Mock
@@ -21,6 +40,8 @@ public class AuthControllerTest {
     private PasswordEncoder passwordEncoder;
     @MockBean
     private JwtUtils jwtUtils;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     private Role role;
     private UserEntity user;
@@ -46,5 +67,33 @@ public class AuthControllerTest {
         loginRequest = new LoginRequest();
         loginRequest.setUsername("testuser");
         loginRequest.setPassword("testpassword");
+    }
+
+    @Test
+    public void AuthController_RegisterUser_ReturnsString() throws Exception {
+        when(userRepository.existsByEmail(Mockito.anyString())).thenReturn(false);
+        when(userRepository.existsByUsername(Mockito.anyString())).thenReturn(false);
+        when(passwordEncoder.encode(Mockito.anyString())).thenReturn("testpassword");
+        when(roleRepository.findByAuthority(Mockito.anyString())).thenReturn(Optional.of(role));
+        when(userRepository.save(Mockito.any(UserEntity.class))).thenReturn(user);
+
+        mockMvc.perform(post("/api/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(registerRequest)))
+                .andExpect(status().isOk())
+                .andExpect(content().string("User registered successfully"));
+    }
+
+    @Test
+    public void AuthController_LoginUser_ReturnsString() throws Exception {
+        when(userRepository.findByUsername(Mockito.anyString())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(Mockito.anyString(), Mockito.anyString())).thenReturn(true);
+        when(jwtUtils.generateToken(Mockito.anyString(), Mockito.any(Role.class))).thenReturn("testtoken");
+
+        mockMvc.perform(post("/api/auth/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(loginRequest)))
+                .andExpect(status().isOk())
+                .andExpect(content().string("testtoken"));
     }
 }
